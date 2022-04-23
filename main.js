8301cfd86bca465e5c03766314f68e5cc5762ae9
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-04-23 15:01:05 +0530

    part 3 completed

--- main.js
+++ main.js
@@ -1,48 +1,70 @@
 // important variables
-quickDrawSet=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye","eyeglasses","face","fan","feather","fence","finger","fire hydrant","fireplace","firetruck","fish","flamingo","flashlight","flip flops","floor lamp","flower","flying saucer","foot","fork","frog","frying pan","garden","garden hose","giraffe","goatee","golf club","grapes","grass","guitar","hamburger","hammer","hand","harp","hat","headphones","hedgehog","helicopter","helmet","hexagon","hockey puck","hockey stick","horse","hospital","hot air balloon","hot dog","hot tub","hourglass","house","house plant","hurricane","ice cream","jacket","jail","kangaroo","key","keyboard","knee","knife","ladder","lantern","laptop","leaf","leg","light bulb","lighter","lighthouse","lightning","line","lion","lipstick","lobster","lollipop","mailbox","map","marker","matches","megaphone","mermaid","microphone","microwave","monkey","moon","mosquito","motorbike","mountain","mouse","moustache","mouth","mug","mushroom","nail","necklace","nose","ocean","octagon","octopus","onion","oven","owl","paintbrush","paint can","palm tree","panda","pants","paper clip","parachute","parrot","passport","peanut","pear","peas","pencil","penguin","piano","pickup truck","picture frame","pig","pillow","pineapple","pizza","pliers","police car","pond","pool","popsicle","postcard","potato","power outlet","purse","rabbit","raccoon","radio","rain","rainbow","rake","remote control","rhinoceros","rifle","river","roller coaster","rollerskates","sailboat","sandwich","saw","saxophone","school bus","scissors","scorpion","screwdriver","sea turtle","see saw","shark","sheep","shoe","shorts","shovel","sink","skateboard","skull","skyscraper","sleeping bag","smiley face","snail","snake","snorkel","snowflake","snowman","soccer ball","sock","speedboat","spider","spoon","spreadsheet","square","squiggle","squirrel","stairs","star","steak","stereo","stethoscope","stitches","stop sign","stove","strawberry","streetlight","string bean","submarine","suitcase","sun","swan","sweater","swingset","sword","syringe","table","teapot","teddy-bear","telephone","television","tennis racquet","tent","The Eiffel Tower","The Great Wall of China","The Mona Lisa","tiger","toaster","toe","toilet","tooth","toothbrush","toothpaste","tornado","tractor","traffic light","train","tree","triangle","trombone","truck","trumpet","tshirt","umbrella","underwear","van","vase","violin","washing machine","watermelon","waterslide","whale","wheel","windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"]
-randomNo = Math.floor((Math.random()*quickDrawSet.length)+1)
+quickDrawSet=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye","eyeglasses","face","fan","feather","fence","finger","fire hydrant","fireplace","firetruck","fish","flamingo","flashlight","flip flops","floor lamp","flower","flying saucer","foot","fork","frog","frying pan","garden","garden hose","giraffe","goatee","golf club","grapes","grass","guitar","hamburger","hammer","hand","harp","hat","headphones","hedgehog","helicopter","helmet","hexagon","hockey puck","hockey stick","horse","hospital","hot air balloon","hot dog","hot tub","hourglass","house","house plant","hurricane","ice cream","jacket","jail","kangaroo","key","keyboard","knee","knife","ladder","lantern","laptop","leaf","leg","light bulb","lighter","lighthouse","lightning","line","lion","lipstick","lobster","lollipop","mailbox","map","marker","matches","megaphone","mermaid","microphone","microwave","monkey","moon","mosquito","motorbike","mountain","mouse","moustache","mouth","mug","mushroom","nail","necklace","nose","ocean","octagon","octopus","onion","oven","owl","paintbrush","paint can","palm tree","panda","pants","paper clip","parachute","parrot","passport","peanut","pear","peas","pencil","penguin","piano","pickup truck","picture frame","pig","pillow","pineapple","pizza","pliers","police car","pond","pool","popsicle","postcard","potato","power outlet","purse","rabbit","raccoon","radio","rain","rainbow","rake","remote control","rhinoceros","rifle","river","roller coaster","rollerskates","sailboat","sandwich","saw","saxophone","school bus","scissors","scorpion","screwdriver","sea turtle","see saw","shark","sheep","shoe","shorts","shovel","sink","skateboard","skull","skyscraper","sleeping bag","smiley face","snail","snake","snorkel","snowflake","snowman","soccer ball","sock","speedboat","spider","spoon","spreadsheet","square","squiggle","squirrel","stairs","star","steak","stereo","stethoscope","stitches","stop sign","stove","strawberry","streetlight","string bean","submarine","suitcase","sun","swan","sweater","swingset","sword","syringe","table","teapot","teddy-bear","telephone","television","tennis racquet","tent","The Eiffel Tower","The Great Wall of China","The Mona Lisa","tiger","toaster","toe","toilet","tooth","toothbrush","toothpaste","tornado","tractor","traffic light","train","tree","triangle","trombone","truck","trumpet","tshirt","umbrella","underwear","van","vase","violin","washing machine","watermelon","waterslide","whale","wheel","windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"];
+randomNo = Math.floor((Math.random()*quickDrawSet.length)+1);
+console.log(quickDrawSet[randomNo]);
 sketch = quickDrawSet[randomNo];
+document.getElementById("sketchToBeDrawn").innerHTML = "Sketch To Be Drawn: " + sketch;
 timerCounter = 0;
-timerCheck;
-drawnSketch;
-answerHolder;
-score = 0;
+timerCheck = '';
+drawnSketch = '';
+answerHolder = '';
+var score = 0;
 function preload(){
-
+   classifier = ml5.imageClassifier('DoodleNet');
 }
 function setup() {
    canvas = createCanvas(280,280);
    canvas.center();
    background("white");
+   canvas.mouseReleased(classifyCanvas);
 }
 function draw() {
-    if(drawnSketch = sketch){
+  strokeWeight(5);
+  stroke(0);
+  if(mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
+  checkSketch();
+    if(drawnSketch == sketch){
         answerHolder = "set";
         score = score + 1;
         document.getElementById("score").innerHTML = "Score : " + score;
     }
-    function checkSketch() {
-        timerCounter = timerCounter + 1;
-        document.getElementById("timer").innerHTML = "Timer : " + timerCounter;
-        console.log(timerCounter);
-        if(timerCounter > 400){
-            timerCounter = 0;
-            timerCheck = "completed";
-        }
-        if(timerCheck = "completed"){
-            timerCheck = "";
-            answerHolder = "";
-            updateCanvas();
-        }
-       else if(answerHolder == "set"){
-            timerCheck = "";
-            answerHolder = "";
-            updateCanvas();
-        }
-    }
+   
 
 }
+function checkSketch() {
+  timerCounter = timerCounter + 1;
+  document.getElementById("timer").innerHTML = "Timer : " + timerCounter;
+  console.log(timerCounter);
+  if(timerCounter > 400){
+      timerCounter = 0;
+      timerCheck = "completed";
+  }
+  if(timerCheck == "completed" || answerHolder == "set" ){
+      timerCheck = "";
+      answerHolder = "";
+      updateCanvas();
+  }
+}
+function classifyCanvas() {
+    classifier.classify(canvas,gotResults)
+  }
 function updateCanvas() {
     background("white");
-    document.getElementById("sketchToBeDrawn").innerHTML = "SKetch To be drawn: " + sketch;
+    randomNo = Math.floor((Math.random()*quickDrawSet.length)+1);
+    sketch = quickDrawSet[randomNo];
+    document.getElementById("sketchToBeDrawn").innerHTML = "Sketch To be drawn: " + sketch;
 }
+function gotResults(e,r){
+    if(e){
+      console.error(e);
+    }
+    else{
+      console.log(r);
+      drawnSketch = r[0].label;
+      confidence = Math.round(r[1].confidence*100)+'%';
+      document.getElementById("currentSketch").innerHTML = 'Your Sketch:'+drawnSketch;
+      document.getElementById('confidence').innerHTML = confidence;
+    }
+  }
